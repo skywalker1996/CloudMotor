21b0c8ea184573b0d27a418ad4ae57f323930215
zjhui <244135822@qq.com>
2021-11-18 23:29:49 +0800

    修改config文件读取模块

--- configs/ConfigLoader.py
+++ configs/ConfigLoader.py
@@ -9,20 +9,24 @@ class ConfigLoader:
     def __init__(self):
         self.global_config = yaml.load(open(path+'/global.yaml'), Loader=yaml.FullLoader)
 
+        self.config_file = self.global_config["configs"]
+
+        self.configs = yaml.load(open(path+'/'+self.config_file), Loader=yaml.FullLoader)
+
     def get_server_address(self):
-        address = self.global_config["server"]["address"]
+        address = self.configs["server"]["address"]
         return address["ip"], address["port"]
 
     def get_client_address(self):
-        address = self.global_config["client"]["address"]
+        address = self.configs["client"]["address"]
         return address["ip"], address["port"]
 
     def get_register_address(self):
-        address = self.global_config["register"]["address"]
+        address = self.configs["register"]["address"]
         return address["ip"], address["port"]
 
     def get_client_ip(self):
-        return self.global_config["client"]["address"]["ip"]
+        return self.configs["client"]["address"]["ip"]
 
 
 
